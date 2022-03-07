0d96b66f7817fd987b0606b5de6da9b6845520b0
tomato <servicedake@gmail.com>
2022-03-08 00:22:47 +0900

    ヨドバシをjsonとcsvで保存可能に

---
+++ yodobashi/yodobashi/spiders/desktop.py
@@ -0,0 +1,27 @@
+import scrapy
+import logging
+
+class DesktopSpider(scrapy.Spider):
+    name = 'desktop'
+    allowed_domains = ['www.yodobashi.com']
+    start_urls = ['https://www.yodobashi.com/category/19531/11970/34646/']
+
+    def parse(self, response):
+        logging.info(response.url)
+        products = response.xpath('//div[contains(@class,"productListTile")]')
+
+        for product in products:
+            maker = product.xpath('.//div[contains(@class,"pName")]/p[1]/text()').get()
+            name = product.xpath('.//div[contains(@class,"pName")]/p[2]/text()').get()
+            price = product.xpath('.//span[contains(@class,"productPrice")]/text()').get()
+
+            yield {
+                'maker': maker,
+                'name': name,
+                'price': price
+            }
+
+        next_page = response.xpath("//a[@class='next']")
+        if next_page:
+            yield response.follow(url=next_page[0], callback=self.parse)
+
